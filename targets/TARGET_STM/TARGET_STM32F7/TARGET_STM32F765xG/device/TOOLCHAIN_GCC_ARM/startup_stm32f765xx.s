2c4d161264073f9eb3b8804953df525f6775f004
Daniel Mancuso <damancuso@gmail.com>
2017-06-08 00:17:04 +0200

    Fix problem with initialization, mbed redirects main()!

--- targets/TARGET_STM/TARGET_STM32F7/TARGET_STM32F765xG/device/TOOLCHAIN_GCC_ARM/startup_stm32f765xx.s
+++ targets/TARGET_STM/TARGET_STM32F7/TARGET_STM32F765xG/device/TOOLCHAIN_GCC_ARM/startup_stm32f765xx.s
@@ -58,10 +58,12 @@ defined in linker script */
 .word  _sdata
 /* end address for the .data section. defined in linker script */
 .word  _edata
+
 /* start address for the .bss section. defined in linker script */
-.word  _sbss
+//.word  _sbss
 /* end address for the .bss section. defined in linker script */
-.word  _ebss
+//.word  _ebss
+
 /* stack used for SystemInit_ExtMemCtl; always internal RAM used */
 
 /**
@@ -95,10 +97,10 @@ LoopCopyDataInit:
   adds  r2, r0, r1
   cmp  r2, r3
   bcc  CopyDataInit
-  ldr  r2, =_sbss
-  b  LoopFillZerobss
+//  ldr  r2, =_sbss
+// b  LoopFillZerobss
 /* Zero fill the bss segment. */  
-FillZerobss:
+/*FillZerobss:
   movs  r3, #0
   str  r3, [r2], #4
     
@@ -106,13 +108,15 @@ LoopFillZerobss:
   ldr  r3, = _ebss
   cmp  r2, r3
   bcc  FillZerobss
+ */ 
 
 /* Call the clock system initialization function.*/
   bl  SystemInit   
 /* Call static constructors */
-    bl __libc_init_array
+  //  bl __libc_init_array
 /* Call the application's entry point.*/
-  bl  main
+  //bl  main
+  bl _start	
   bx  lr    
 .size  Reset_Handler, .-Reset_Handler
 
